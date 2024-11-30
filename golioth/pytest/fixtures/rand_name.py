f92a46554b762df4ef818eb4fa6405ed3253d668
Mike Szczys <mike@golioth.io>
2024-11-30 16:54:11 -0600

    fixtures: rand_name: allow optional custom prefix to be used
    Signed-off-by: Mike Szczys <mike@golioth.io>

--- golioth/pytest/fixtures/rand_name.py
+++ golioth/pytest/fixtures/rand_name.py
@@ -1,7 +1,10 @@
 import random
 import string
 
-NAME_PREFIX = 'pytest-'
+NAME_PREFIX = 'pytest'
 
-def get_random_name():
-    return NAME_PREFIX + ''.join(random.choice(string.ascii_lowercase + string.digits) for i in range(16))
+def get_random_name(prefix: str | None = None):
+    random_string = ''.join(random.choice(string.ascii_lowercase + string.digits)
+                            for _ in range(16))
+
+    return f'{prefix or NAME_PREFIX}-{random_string}'
